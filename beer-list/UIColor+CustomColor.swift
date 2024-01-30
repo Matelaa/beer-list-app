6824287f83342acce4ab815cdb739dbafb604528
Matelaa <jmatelaneto@gmail.com>
2024-01-30 13:56:29 -0300

    importing the colors from design and setting the launchscreen

---
+++ beer-list/UIColor+CustomColor.swift
@@ -0,0 +1,30 @@
+//
+//  UIColor+CustomColor.swift
+//  beer-list
+//
+//  Created by Matela on 30/01/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static var lightBlue: UIColor {
+        return UIColor(red: 0.29, green: 0.56, blue: 0.89, alpha: 1.00)
+    }
+    
+    static var darkBlue: UIColor {
+        return UIColor(red: 0.16, green: 0.31, blue: 0.49, alpha: 1.00)
+    }
+    
+    static var lightGray: UIColor {
+        return UIColor(red: 0.40, green: 0.40, blue: 0.40, alpha: 1.00)
+    }
+    
+    static var primaryWhite: UIColor {
+        return UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)
+    }
+    
+    static var secondaryWhite: UIColor {
+        return UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.00)
+    }
+}
