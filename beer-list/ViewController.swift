cca3ff9c6003401426a4e4755a5bce2b279016e1
Matelaa <jmatelaneto@gmail.com>
2024-01-30 13:32:47 -0300

    removing storyboard from project

--- beer-list/ViewController.swift
+++ beer-list/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.text = "Hello World"
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
+        self.view.addSubview(self.label)
+        NSLayoutConstraint.activate([
+            self.label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
-
-
 }
 
