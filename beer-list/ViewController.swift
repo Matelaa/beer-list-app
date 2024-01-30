40ddb1a3a46282ac56d7c11a564e14feb7fa3332
Matelaa <jmatelaneto@gmail.com>
2024-01-30 13:13:52 -0300

    Initial Commit

---
+++ beer-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beer-list
+//
+//  Created by Matela on 30/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
